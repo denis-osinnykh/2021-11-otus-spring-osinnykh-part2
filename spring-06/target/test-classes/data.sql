aeb6faa015e3084823e548a479478f97c261a160
Denis Osinnykh <94908596+denis-osinnykh@users.noreply.github.com>
2022-03-05 11:24:56 +0500

    Delete spring-06/target directory

--- spring-06/target/test-classes/data.sql
+++
@@ -1,9 +0,0 @@
-insert into author (name) values ('Test author');
-
-insert into author (name) values ('Test author 2');
-
-insert into genre (name) values ('Test genre');
-
-insert into genre (name) values ('Test genre 2');
-
-insert into book (name, author_id, genre_id) values ('Test book', 1, 1);
